3e80113d8452355dd809ba7ea2d405301f2e9d5d
Moses Mbugua <126574366+Moses-Mbugs@users.noreply.github.com>
2025-12-22 09:30:06 +0300

    Fix React 18 root rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
-import './index.css';
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 import { App } from "./App";
-render(<App />, document.getElementById("root"));
+import "./index.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
